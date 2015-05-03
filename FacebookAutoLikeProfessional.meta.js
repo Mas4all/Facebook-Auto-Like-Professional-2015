12affde0bc9aba5612b384d504cc2ab4a58ae8e5
Mas4all <mas4all@yahoo.com>
2015-05-03 23:35:38 +0500

    Create FacebookAutoLikeProfessional.meta.js

---
+++ FacebookAutoLikeProfessional.meta.js
@@ -0,0 +1,5 @@
+// ==UserScript==
+// @namespace		http://alltipsotricks.blogspot.com
+// @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeProfessional.user.js
+// @version  		13.0
+// ==/UserScript==
