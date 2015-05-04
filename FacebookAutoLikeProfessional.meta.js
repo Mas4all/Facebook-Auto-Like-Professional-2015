8e8c81419d37d76d22b6fa9223277e0c29477b98
Mas4all <mas4all@yahoo.com>
2015-05-04 11:55:34 +0500

    Update FacebookAutoLikeProfessional.meta.js

--- FacebookAutoLikeProfessional.meta.js
+++ FacebookAutoLikeProfessional.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @namespace		http://alltipsotricks.blogspot.com
-// @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeProfessional.user.js
+// @downloadURL 	https://github.com/Mas4all/Facebook-Auto-Like-Professional-2015/raw/master/FacebookAutoLikeProfessional.user.js
 // @version  		13.0
 // ==/UserScript==
